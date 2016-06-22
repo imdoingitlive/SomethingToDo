87af5bd328711e383dc6518a8eeeec144b612ac0
Edwin Rivera <edwin.rivera@outlook.com>
2016-06-22 19:51:59 -0400

    updated homepage

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+		$("#searchBox").on("click", function(){
+		 	// Get inputs
+		 	var location = $('#cityZip').val(); 
+				
+		 	console.log(location);
+
+			return false;
+	   	});
+});
