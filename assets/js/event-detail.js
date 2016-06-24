739cbcdcd7e61191a3f066e1a2c40de4e73e0768
Stefanie Centi <stefcenti@centiumtech.com>
2016-06-24 02:09:50 -0400

    added a list of restaurants with details; improved the display of the event detail and the restaurants.

--- assets/js/event-detail.js
+++ assets/js/event-detail.js
@@ -4,20 +4,38 @@
 
  https://app.ticketmaster.com/discovery/v2/events/G5diZf7xE5vpO.json?apikey=OmRbi4xJQVJOYo6jqYeDimlPMGA8UvXS
 
+ * Use the OpenTable API to access a list of restaurants in
+ * the general vinciy
+ *
+ * http://opentable.herokuapp.com/api
+ *
  ****/
 
- var eventID = "G5diZf7xE5vpO.json";
- var tmApiKey = "OmRbi4xJQVJOYo6jqYeDimlPMGA8UvXS";
- 
- function test(){
- 	console.log("test");
+/*------  TEMPORARY CODE  ----------------
+/* Comment out temporary code to test with 
+/* integrated app.
+/*-----*/
+// Temporary variables so to hold hardcoded id and key for testing
+var eventID = "G5diZf7xE5vpO.json";
+var tmApiKey = "OmRbi4xJQVJOYo6jqYeDimlPMGA8UvXS";
 
- 	eventDetail(eventID);
+function test(){
+	console.log("test");
 
- 	$('.ui.modal').modal('show');
+	eventDetail(eventID);
 
- 	$('.ui.accordion').accordion();
+	$('.ui.modal').modal('show');
+
+	$('.ui.accordion').accordion();
  };
+
+test();
+
+/* -------  END TEMPORARY CODE --------- */
+
+/*****
+ * Event Detail Functions
+ *****/
 
 function eventDetail(eventID) {
 
@@ -39,9 +57,14 @@ function eventDetail(eventID) {
 		 	/*--------------------------------------*/
 		 	/* Display the Event Name in the Heading
 		 	/*--------------------------------------*/
-			$('.header').html(tmData.name);
+			$('#modal-header').html(tmData.name);
 
+			/*-------------*/
+			/* Display sub-headings
 		 	/*-------------*/
+		 	$('left-column').append("<h5>Event Details</h5>");
+		 	$('right-column').append("<h5>Nearby Restaurants</h5>");
+
 		 	/* Display an Image for the Event
 		 	/*-------------*/
 		 	displayImage(tmData.images);
@@ -56,12 +79,22 @@ function eventDetail(eventID) {
 		 	/*-------------*/
 		 	displayDate(tmData);
 
+		 	/*-------------*/
+		 	/* Display a list of Restaurants in the area
+		 	/*-------------*/
+		 	if (tmData._embedded.venues.length > 0) {
+		 		var zipcode = tmData._embedded.venues[0].postalCode;
+		 		restaurantList(zipcode);
+		 	}
+
 		});
 };
 
 function displayImage(images) {
 
 	var imageDiv = $('<img>');
+	imageDiv.addClass('image');
+//	imageDiv.addClass('ui segment');
 	imageDiv.addClass('event-detail-img');
 
 	// Search for the image where the following keys are found:
@@ -111,57 +144,17 @@ function displayVenue(venue, isActive) {
 
 	titleDiv.append(iconDiv);
 	titleDiv.append(venue.name);
+
 	innerContentDiv.append(venue.city.name + ", " + venue.state.stateCode);
-var url = '<a href="' + venue.url + '">More Info...</a>';
-//	innerContentDiv.append('<a href="' + venue.url + '"></a>');
+
+	var url = '<a href="' + venue.url + '">More Info...</a>';
 	innerContentDiv.append("<br>" + url);
+
 	outerContentDiv.append(innerContentDiv);
 
-	$('.venue.content.ui.styled.accordion').append(titleDiv);
+	$('.venue.content.ui.accordion').append(titleDiv);
 
-	$('.venue.content.ui.styled.accordion').append(outerContentDiv);
-/*
-  <div class="title active">
-    <i class="dropdown icon"></i>
-    What is a dog?
-  </div>
-  <div class="content active">
-    <p class="transition visible" style="display: block !important;">A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-*/
- /*****
-
-Sample code for accordian styled container.
-
-Show the detail for the first venue in the list.
-
-Other venues will be collapsed.
-
-<div class="ui styled accordion">
-  <div class="title active">
-    <i class="dropdown icon"></i>
-    What is a dog?
-  </div>
-  <div class="content active">
-    <p class="transition visible" style="display: block !important;">A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-  </div>
-  <div class="title">
-    <i class="dropdown icon"></i>
-    What kinds of dogs are there?
-  </div>
-  <div class="content">
-    <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
-  </div>
-  <div class="title">
-    <i class="dropdown icon"></i>
-    How do you acquire a dog?
-  </div>
-  <div class="content">
-    <p class="transition hidden">Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
-    <p class="transition hidden">A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
-  </div>
-</div>
-
-****/
+	$('.venue.content.ui.accordion').append(outerContentDiv);
 
 }
 
@@ -170,10 +163,10 @@ function displayDate(event) {
 	var date = moment(event.dates.start.dateTime).format("MM/DD/YY hh:mm A");
 	var saleDate = moment(event.sales.public.startDateTime).format("MM/DD/YY hh:mm A");
 
-	date = "<h5>Event Date: </h5>" + date + "<br>";
-	saleDate = "<h5>On Sale to General Public Starting: </h5>" + saleDate + "<br>";
+	date = "<p>Event Date: " + date + "</p>";
+	saleDate = "<p>On Sale Starting: " + saleDate + "</p>";
 
-	$('#image-content').append('<div class="ui segment" id="event-content"></div>');
+	$('#image-content').append('<div class="content" id="event-content"></div>');
 
 	$('#event-content').append(date);
 	$('#event-content').append(saleDate);
@@ -199,12 +192,89 @@ function displayPriceRange(prices) {
 		}
 	}
 	
-	var priceRange = "<h5>Prices Range From: </h5>" + min + " - " + max + " " + currency + "<br>";
+	var priceRange = "<p>Prices Range From: " + min + " - " + max + " " + currency + "</p><br>";
 
 	$('#event-content').append(priceRange);
 }
-$('.venue.content.ui.styled.accordion').on("click", function(title) {
 
+$('.venue.content.ui.accordion').on("click", function(title) {
+	// Add functionality to re-display restaurant list
+	// when venue is selected
 });
 
-test();
+/*****
+ * Restaurant Functions
+ *****/
+function restaurantList(zipcode) {
+
+ 	/*-------------*/
+ 	/* 1: Get list of restaurants from the OpenTable API
+ 	/*-------------*/
+	var otQueryURL="https://opentable.herokuapp.com/api/restaurants?zip=" + zipcode;
+
+ 	// The AJAX function uses the URL and Gets the JSON data associated with it. 
+ 	// The data then gets stored in the variable called: "otData"
+	$.ajax({url: otQueryURL, method: "GET"}) 
+		.done(function(otData) {
+			console.log(otQueryURL);
+			console.log(otData);
+
+		 	/*-------------*/
+		 	/* 2: Use an accordion element to display the first 5
+		 	/* restaurants in the list of restaurants returned from the API.
+		 	/* The first restaurant's info will be active.
+		 	/*-------------*/
+		 	for (var i = 0; i < otData.restaurants.length && i < 5; i++) {
+				/* The first restaurant found will be active */
+				var r = otData.restaurants[i];
+				displayRestaurant(otData.restaurants[i], i == 0 ? true : false);
+			}
+		});
+}
+
+function displayRestaurant(restaurant, isActive) {
+
+	var titleDiv = $('<div>');
+	var iconDiv = $('<i>');
+	var outerContentDiv = $('<div>');
+	var innerContentDiv = $('<p>');
+
+	if (isActive) {
+		titleDiv.addClass('title active');
+		outerContentDiv.addClass('content active');
+		innerContentDiv.addClass('transition visible');
+		innerContentDiv.attr('style', 'display: block !important;');
+	} else {
+		titleDiv.addClass('title');
+		outerContentDiv.addClass('content');
+		innerContentDiv.addClass('transition hidden');
+	}
+
+	iconDiv.addClass('dropdown icon');
+
+	// Display restaurant name in the title
+	titleDiv.append(iconDiv);
+	titleDiv.append(restaurant.name);
+
+	// Display an image from the restaurant
+	var imageDiv = $('<img>');
+	imageDiv.addClass('restaurant-img');
+	imageDiv.attr('src', restaurant.image_url);
+	innerContentDiv.append(imageDiv);
+
+	innerContentDiv.append("<br>" +
+							restaurant.address + "<br>" + 
+							restaurant.city + ", " + 
+							restaurant.state + ", " + 
+							restaurant.postal_code);
+
+	var url = '<a href="' + restaurant.reserve_url + '">Reservations...</a>';
+	innerContentDiv.append("<br>" + url);
+
+	outerContentDiv.append(innerContentDiv);
+
+	$('.restaurant.content.ui.accordion').append(titleDiv);
+
+	$('.restaurant.content.ui.accordion').append(outerContentDiv);
+
+}
