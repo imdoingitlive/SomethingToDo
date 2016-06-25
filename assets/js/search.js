09d3061612b4c7127cd5a9cc604f1cc087497c5c
Edwin Rivera <edwin.rivera@outlook.com>
2016-06-25 10:26:48 -0400

    adding code

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,18 +3,18 @@ $( document ).ready(function() {
 	var userInfo = $("#zipcode").val();
 	console.log(unserInfo);
 
-	$("#searchForm").validate({
-		rules: {
-			zipcode: {
-			required: true,
-			minlength: 5,
-			maxlength: 5,
-			zipcodeUS: true
-			}
-		}
-	});
+	// $("#searchForm").validate({
+	// 	rules: {
+	// 		zipcode: {
+	// 		required: true,
+	// 		minlength: 5,
+	// 		maxlength: 5,
+	// 		zipcodeUS: true
+	// 		}
+	// 	}
+	// });
 
 	$('#zipcode').click(function(){
-   		window.location.href='eventlist.html?startDateTime=2016-06-18T00:00:00Z&postalCode=' + userInfo;
+   		window.location.href='eventlist.html?startDateTime=2016-06-18T00:00:00Z&postalCode=10001';
 	})
 });
